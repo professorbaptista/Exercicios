03a7967345d6b434a33dada3d56bce95fac7501c
professorbaptista <josbaptista28@yahoo.com.br>
2023-12-26 01:40:46 +0000

    reajuste

--- estilo.js
+++ estilo.js
@@ -2,22 +2,22 @@
 
 carregar = () =>{
     let img = document.getElementById ('img')
-    let img1 = document.getElementById ('imagem')
+    let img1 = document.getElementById ('image')
     let data = new Date()
-    let hora = data.getHours()
-    
+    //let hora = data.getHours()
+    let hora = 4
   
     img.innerHTML = `Agora são ${hora} horas`
     //Condição
     if((hora >= 0) && (hora < 12)){
 
         //Bom Dia
-        img1.src ='manha-1.png'
+        img1.src ='foto-2.webp'
     }
     else if((hora >= 12) && (hora < 18)){
         // Boa Tarde
 
-        img1.src='template.png'
+        img1.src='foto-3.webp'
     }
     else{
         //Boa Noite
